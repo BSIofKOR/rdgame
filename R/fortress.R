93621a0c2d061f298ec98456dd9b54aab37447f2
BSIofKOR <z0564@naver.com>
2018-05-23 11:13:11 +0900

    test~!!!!

--- R/fortress.R
+++ R/fortress.R
@@ -19,11 +19,10 @@ g <- 9.8
 
 # function
 
-
 player <- function(speed, radian) {
   if (speed > 0 & radian > 0 & radian < pi/2) {
-    v <<- speed
-    theta <<- radian
+    v <<- speed + runif(1, 0, 0.1)
+    theta <<- radian + runif(1, 0, 0.1)
 
     # Parabolic formula
     x <- v*cos(theta)*t
@@ -35,6 +34,7 @@ player <- function(speed, radian) {
     # Visualization
     df <- data.frame(x, y)
     ggplot(data = df, aes(x = x, y = y)) +
+      coord_cartesian(xlim = c(0,10000), ylim = c(0, 5000))
       geom_line(linetype = 5, size = 1, colour = "blue")
 
   } else {
@@ -42,4 +42,7 @@ player <- function(speed, radian) {
     print("The angle must be greater than zero and smaller than 1.57 radian!")
   }
 }
+player(150, 1.34)
 
+
+}

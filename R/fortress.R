b6b8d0ba36f8e3ef378ad78afaecef7121c389a8
BSIofKOR <z0564@naver.com>
2018-05-23 11:35:29 +0900

    5/23 saved

--- R/fortress.R
+++ R/fortress.R
@@ -20,7 +20,7 @@ g <- 9.8
 # function
 
 player <- function(speed, radian) {
-  if (speed > 0 & speed < 1000 & radian > 0 & radian < pi/2) {
+  if ((speed > 0) & (speed < 1000) & (radian > 0) & (radian < pi/2)) {
     v <<- speed + runif(1, 0, 0.1)
     theta <<- radian + runif(1, 0, 0.1)
 
@@ -38,8 +38,8 @@ player <- function(speed, radian) {
       ggplot2::geom_line(linetype = 5, size = 1, colour = "blue")
 
   } else {
-    v <<- 500
-    theta <<- 0.5
+    v <<- 900
+    theta <<- 1
 
     # Parabolic formula
     x <- v*cos(theta)*t
@@ -52,13 +52,13 @@ player <- function(speed, radian) {
     df <- data.frame(x, y)
     ggplot2::ggplot(data = df, aes(x = x, y = y)) +
       ggplot2::coord_cartesian(xlim = c(0,10000), ylim = c(0, 5000)) +
-      ggplot2::geom_line(linetype = 5, size = 1, colour = "blue")
+      ggplot2::geom_line(linetype = 5, size = 1, colour = "red")
 
     print("The Speed must be greater than zero adn smaller then 1000! &")
     print("The angle must be greater than zero and smaller than 1.57 radian!")
   }
 }
-player(1500, 1)
+player(0, 1)
 
 
 

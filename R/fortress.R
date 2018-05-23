a1eed6ddb5414e0b9b91db170d200d4b638627f7
BSIofKOR <z0564@naver.com>
2018-05-23 10:42:00 +0900

    test~!~!!

--- R/fortress.R
+++ R/fortress.R
@@ -18,31 +18,28 @@ t <- seq(0, 100, 0.1)
 g <- 9.8
 
 # function
-player <- function(a, b) {
-  rdelement <<- runif(1, -10, 10)
-  if (a > 0) {
-    v <<- a + runif(1, -10, 10 + rdelement)
-  } else {
-    print("The Speed must be greater than zero!")
-  }
 
-  if (b > 0 & b < pi/2) {
-    theta <<- b + runif(1, -0.1, 0.1)
+
+player <- function(speed, radian) {
+  if (speed > 0 & radian > 0 & radian < pi/2) {
+    v <<- speed
+    theta <<- radian
+
+    # Parabolic formula
+    x <- v*cos(theta)*t
+    y <- v*sin(theta)*t - 0.5*g*t^2
+
+    x <- x[y >= 0]
+    y <- y[y >= 0]
+
+    # Visualization
+    df <- data.frame(x, y)
+    ggplot(data = df, aes(x = x, y = y)) +
+      geom_line(linetype = 5, size = 1, colour = "blue")
+
   } else {
+    print("The Speed must be greater than zero! and")
     print("The angle must be greater than zero and smaller than 1.57 radian!")
   }
-
-  x <- v*cos(theta)*t
-  y <- v*sin(theta)*t - 0.5*g*t^2
-
-  x <- x[y >= 0]
-  y <- y[y >= 0]
-
-  df <- data.frame(x, y)
-  ggplot(data = df, aes(x = x, y = y)) +
-    geom_line(linetype = 5, size = 1, colour = "blue")
-
 }
-
-player(150, 1.4)
 

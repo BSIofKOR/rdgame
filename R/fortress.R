dc038024837104d73c5ec62a64672a5868ca0ca2
BSIofKOR <z0564@naver.com>
2018-05-30 09:07:54 +0900

    asdf

--- R/fortress.R
+++ R/fortress.R
@@ -1,7 +1,7 @@
 #' similar fortress game
 #'
-#' @param a speed input
-#' @param b angle input
+#' @param speed speed input
+#' @param angle input
 #' @return the distance of \code{a} and \code{b}
 #' @examples
 #' player(100, 1)
@@ -18,47 +18,33 @@ t <- seq(0, 100, 0.1)
 g <- 9.8
 
 # function
-
 player <- function(speed, radian) {
   if ((speed > 0) & (speed < 1000) & (radian > 0) & (radian < pi/2)) {
-    v <<- speed + runif(1, 0, 0.1)
-    theta <<- radian + runif(1, 0, 0.1)
+    v <- speed
+    theta <- radian
 
     # Parabolic formula
-    x <- v*cos(theta)*t
-    y <- v*sin(theta)*t - 0.5*g*t^2
+    xx <- v*cos(theta)*t
+    yy <- v*sin(theta)*t - 0.5*g*t^2
 
-    x <- x[y >= 0]
-    y <- y[y >= 0]
+    xx <- xx[yy >= 0]
+    yy <- yy[yy >= 0]
 
     # Visualization
-    df <- data.frame(x, y)
-    ggplot2::ggplot(data = df, aes(x = x, y = y)) +
-      ggplot2::coord_cartesian(xlim = c(0,10000), ylim = c(0, 5000)) +
+    df <- data.frame(xx, yy)
+    ggplot2::ggplot(data = df, mapping = aes(xx, yy)) +
+      ggplot2::coord_cartesian(xlim = c(0, 10000), ylim = c(0, 5000)) +
       ggplot2::geom_line(linetype = 5, size = 1, colour = "blue")
 
   } else {
-    v <<- 900
-    theta <<- 1
-
-    # Parabolic formula
-    x <- v*cos(theta)*t
-    y <- v*sin(theta)*t - 0.5*g*t^2
-
-    x <- x[y >= 0]
-    y <- y[y >= 0]
-
-    # Visualization
-    df <- data.frame(x, y)
-    ggplot2::ggplot(data = df, aes(x = x, y = y)) +
-      ggplot2::coord_cartesian(xlim = c(0,10000), ylim = c(0, 5000)) +
-      ggplot2::geom_line(linetype = 5, size = 1, colour = "red")
 
     print("The Speed must be greater than zero adn smaller then 1000! &")
     print("The angle must be greater than zero and smaller than 1.57 radian!")
+
+    return()
   }
 }
-player(0, 1)
-
-
+player(150, 1)
+?ggplot
+ggplot2::ggplot()
 
